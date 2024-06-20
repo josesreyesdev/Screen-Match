b6c35a01771cce49f06be7daf4445899fac60a3b
Jose SR <josesanchez09190@gmail.com>
2024-06-20 01:14:01 -0600

    added returning series in browser

--- screenmatch/src/main/java/com/jsrspring/screenmatch/controller/SeriesController.java
+++ screenmatch/src/main/java/com/jsrspring/screenmatch/controller/SeriesController.java
@@ -6,13 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
 public class SeriesController {
-
-    private List<Series> series;
 
     @Autowired
     private SeriesRepository repository;
