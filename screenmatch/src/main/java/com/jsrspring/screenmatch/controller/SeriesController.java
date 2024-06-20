cb145d731eb02d10a838b591d016f8faf5155e2d
Jose SR <josesanchez09190@gmail.com>
2024-06-20 01:04:52 -0600

    added returning series in browser

--- screenmatch/src/main/java/com/jsrspring/screenmatch/controller/SeriesController.java
+++ screenmatch/src/main/java/com/jsrspring/screenmatch/controller/SeriesController.java
@@ -1,17 +1,28 @@
 package com.jsrspring.screenmatch.controller;
 
+import com.jsrspring.screenmatch.model.Series;
+import com.jsrspring.screenmatch.repository.SeriesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 public class SeriesController {
 
+    private List<Series> series;
 
+    @Autowired
+    private SeriesRepository repository;
 
     @GetMapping("/series")
-    public String showMessage() {
-        return "Hello World";
+    public List<Series> getSeries() {
+        return repository.findAll();
+        /*series = repository.findAll()
+        if (!series.isEmpty()) {
+            return series;
+        } else return Collections.emptyList(); */
     }
-
-
 }
